fc27fe8a1b983af7870b1f73c5eabddef8a12615
BenFoulon <benjamin.foulon62@gmail.com>
2020-11-30 17:28:43 +0100

    Ajout du scrpit conditions.php
    Ajput d'un script qui montre différentes notation en php

---
+++ conditions.php
@@ -0,0 +1,13 @@
+<!-- Dans un fichier 100% php  -->
+<?php
+if (true) {
+    echo "C'est vrai";
+}
+?>
+
+<!-- Notations integrer dans un fichier html -->
+<?php if (true): ?>
+<p>
+C'est vrai
+</p>
+<?php endif ?>
