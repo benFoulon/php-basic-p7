260146e0d165a8b837cbcee4c2c57d10d8db7596
BenFoulon <benjamin.foulon62@gmail.com>
2020-12-02 15:00:38 +0100

    Finalisation de la validations des données du formulaires
    La validation vérifie le login, l'année et l'email

--- form-validation.php
+++ form-validation.php
@@ -3,7 +3,7 @@ use \DateTime;
 
 require __DIR__.'/vendor/autoload.php';
 
-dump($_POST);
+// dump($_POST);
 
 // if ($_POST) {
 //     if(empty($_POST['login'])){
@@ -22,8 +22,15 @@ dump($_POST);
 $errors = [];
 
 if ($_POST) {
+    $minLength = 3;
+    $maxLength = 10;
+
     if (empty($_POST['login'])) {
         $errors['login'] = 'merci de remplir ce champ';
+    } elseif (strlen($_POST['login']) < 3 || strlen($_POST['login']) > 10){
+        $errors['login'] = "merci de renseigner un login dont la longueur est comprise entre {$minLength} et {$maxLength}";
+    } elseif(preg_match('/[a-zA-z]+$/', $_POST['login']) === 0) { 
+        $errors['login'] = "merci de renseigner un login uniquement composé de lettre de l'alphabets sans accent.";
     }
 
     $date = new DateTime();
@@ -37,13 +44,15 @@ if ($_POST) {
         $errors['year'] = 'merci de remplir ce champ avec une année valide';
     } elseif ((float) $_POST['year'] - (int) $_POST['year'] != 0) {
         $errors['year'] = 'merci de remplir ce champ avec une année valide';
-    } elseif ($_POST['year'] < $minYear || $_POST['year' > $maxYear]) {
+    } elseif ($_POST['year'] <= $minYear || $_POST['year'] >= $maxYear) {
         $errors['year'] = "merci de remplir une année comprise entre {$minYear} et {$maxYear} inclus";
     } 
     
 
     if (empty($_POST['email'])) {
         $errors['email'] = 'merci de remplir ce champ';
+    } elseif (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == false ){
+        $errors['email'] = 'merci de renseigner un email valide'; 
     }
 }
 
